eafc00fdd130b9e7f8241b531076cf2669508b52
TSB <tobishittabey@gmail.com>
2024-02-26 11:36:25 +0000

    Initial Commit

---
+++ player-client/player_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  player_clientApp.swift
+//  player-client
+//
+//  Created by TSB M3DIA on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct player_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
