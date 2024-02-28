492f0b9da6456f5906e0a10c4153836d296454fb
TSB <tobishittabey@gmail.com>
2024-02-28 20:15:27 +0000

    [dmg-client] : sdk stable

--- player-client/ContentView.swift
+++ player-client/ContentView.swift
@@ -2,44 +2,46 @@ import SwiftUI
 import dmg_player_ios_sdk
 
 struct ContentView: View {
-    // Assuming your TrackPlayerSDK is designed to be used as an ObservableObject
     @StateObject private var playerSDK = DMGPlayerSDK()
 
     var body: some View {
         VStack {
             DMGPictureLicense(sdk: playerSDK)
-            
-            Text("Now Playing:")
-            Button("Play") {
-                // Example ISRC to play. This should be replaced with actual ISRC values.
-                let isrc = "GBARL2001236"
-                playerSDK.playNow(isrc: isrc)
+            Group {
+                Text("Now Playing:")
+                Button("Play") {
+                    let isrc = "GBARL2001236"
+                    playerSDK.playNow(isrc: isrc)
+                }
+                Button("Queue") {
+                    let isrc = "USANG2208486"
+                    playerSDK.queue(isrc: isrc)
+                }
+                Button("Queue") {
+                    let isrc = "USUM71714139"
+                    playerSDK.queue(isrc: isrc)
+                }
+                Button("Queue") {
+                    let isrc = "USWB10002407"
+                    playerSDK.queue(isrc: isrc)
+                }
+                Button("Print Queue") {
+                    playerSDK.printQueue()
+                }
+                Button("Resume") {
+                    playerSDK.resume()
+                }
+                Button("Pause") {
+                    playerSDK.pause()
+                }
+                Button("Next") {
+                    playerSDK.next()
+                }
+                Button("Stop") {
+                    playerSDK.stop()
+                }
             }
-            Button("Queue") {
-                // Example ISRC to play. This should be replaced with actual ISRC values.
-                let isrc = "USANG2208486"
-                playerSDK.queue(isrc: isrc)
-            }
-            Button("Queue") {
-                // Example ISRC to play. This should be replaced with actual ISRC values.
-                let isrc = "USUG12204888"
-                playerSDK.queue(isrc: isrc)
-            }
-            Button("Queue") {
-                // Example ISRC to play. This should be replaced with actual ISRC values.
-                let isrc = "USWB10002407"
-                playerSDK.queue(isrc: isrc)
-            }
-            Button("Print Queue") {
-                playerSDK.printQueue()
-            }
-            
         }
-        .onAppear {
-            // Setup or initial calls can be made here
-        }
-        
-        
     }
 }
 
