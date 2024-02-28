2aaef1c86e029bd4298520d674069cfa05552df3
TSB1999 <hakeems@quickcommerce.org>
2024-02-28 07:57:11 +0000

    [dmg-client] : new views

--- player-client/ContentView.swift
+++ player-client/ContentView.swift
@@ -3,10 +3,11 @@ import dmg_player_ios_sdk
 
 struct ContentView: View {
     // Assuming your TrackPlayerSDK is designed to be used as an ObservableObject
-    @StateObject private var playerSDK = TrackPlayerSDK()
+    @StateObject private var playerSDK = DMGPlayerSDK()
 
     var body: some View {
         VStack {
+            DMGPictureLicense(sdk: playerSDK)
             
             Text("Now Playing:")
             Button("Play") {
@@ -19,10 +20,20 @@ struct ContentView: View {
                 let isrc = "USANG2208486"
                 playerSDK.queue(isrc: isrc)
             }
+            Button("Queue") {
+                // Example ISRC to play. This should be replaced with actual ISRC values.
+                let isrc = "USUG12204888"
+                playerSDK.queue(isrc: isrc)
+            }
+            Button("Queue") {
+                // Example ISRC to play. This should be replaced with actual ISRC values.
+                let isrc = "USWB10002407"
+                playerSDK.queue(isrc: isrc)
+            }
             Button("Print Queue") {
                 playerSDK.printQueue()
             }
-            WebViewWrapper(sdk: playerSDK)
+            
         }
         .onAppear {
             // Setup or initial calls can be made here
