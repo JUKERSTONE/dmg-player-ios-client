532fa84c8928681a2958bc631c7dc2f3cca6cbeb
TSB <tobishittabey@gmail.com>
2024-03-24 15:45:37 +0000

    [dmg-client] : new structs

--- player-client/ContentView.swift
+++ player-client/ContentView.swift
@@ -7,44 +7,89 @@ struct ContentView: View {
     var body: some View {
         VStack {
             DMGPictureLicense(sdk: playerSDK)
-            Group {
-                Text("Now Playing:")
-                Button("Play") {
-                    let isrc = "GBARL2001236"
-                    playerSDK.playNow(isrc: isrc)
-                }
-                Button("Queue") {
-                    let isrc = "USANG2208486"
-                    playerSDK.queue(isrc: isrc)
-                }
-                Button("Queue") {
-                    let isrc = "USUM71714139"
-                    playerSDK.queue(isrc: isrc)
-                }
-                Button("Queue") {
-                    let isrc = "USWB10002407"
-                    playerSDK.queue(isrc: isrc)
-                }
-                Button("Print Queue") {
-                    playerSDK.printQueue()
-                }
-                Button("Resume") {
-                    playerSDK.resume()
-                }
-                Button("Pause") {
-                    playerSDK.pause()
-                }
-                Button("Next") {
-                    playerSDK.next()
-                }
-                Button("Stop") {
-                    playerSDK.stop()
-                }
-            }
+            nowPlayingGroup
+            controlButtons
+        }
+    }
+    
+    var nowPlayingGroup: some View {
+        Group {
+            Text("Now Playing:")
+        }
+    }
+    
+    var controlButtons: some View {
+        Group {
+            playButton
+            queueButton1
+            queueButton2
+            queueButton3
+            printQueueButton
+//            resumeButton
+//            pauseButton
+            nextButton
+            stopButton
+        }
+    }
+    
+    var playButton: some View {
+        Button("Play") {
+            let isrc = "FRX202363539"
+            playerSDK.playNow(isrc: isrc)
+        }
+    }
+    
+    var queueButton1: some View {
+        Button("Queue") {
+            let isrc = "USQX91402597"
+            playerSDK.queue(isrc: isrc)
+        }
+    }
+    
+    var queueButton2: some View {
+        Button("Queue") {
+            let isrc = "USUM70846400"
+            playerSDK.queue(isrc: isrc)
+        }
+    }
+    
+    var queueButton3: some View {
+        Button("Queue") {
+            let isrc = "USUYG1001593"
+            playerSDK.queue(isrc: isrc)
+        }
+    }
+    
+    var printQueueButton: some View {
+        Button("Print Queue") {
+            playerSDK.printQueue()
+        }
+    }
+    
+//    var resumeButton: some View {
+//        Button("Resume") {
+//            playerSDK.resume()
+//        }
+//    }
+//
+//    var pauseButton: some View {
+//        Button("Pause") {
+//            playerSDK.pause()
+//        }
+//    }
+    
+    var nextButton: some View {
+        Button("Next") {
+            playerSDK.next()
+        }
+    }
+    
+    var stopButton: some View {
+        Button("Stop") {
+            playerSDK.stop()
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
